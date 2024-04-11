ff2a81274fa98d133e3a323d8ae681aac251d6c2
Madatti <101431274+MarcoDBenetti@users.noreply.github.com>
2024-04-11 11:18:32 +0000

    Atualização do exercício conversao_para_hora utilizando operador condicional ternário.

--- exercicios/parte1/conversao_para_hora.c
+++ exercicios/parte1/conversao_para_hora.c
@@ -12,30 +12,20 @@ int main()
     minutos = (segundos - (segundos % 60)) / 60;
     segundos = segundos - (minutos * 60);
 
-    if (horas > 0 && horas > 1) {
-        printf("%d horas", horas);
+    if (horas > 0) {
+        printf("%d %s", horas, horas>1 ? "horas" : "hora");
         if(minutos!=0 || segundos!=0){
             printf(", ");
         }
-    } else if (horas == 1) {
-        printf("%d hora", horas);
-        if(minutos!=0 || segundos!=0){
-            printf(", ");
-        }
-    }
-    if (minutos > 0 && minutos > 1) {
-        printf("%d minutos", minutos);
+    } 
+    if (minutos > 0) {
+        printf("%d %s", minutos, minutos>1 ? "minutos" : "minuto");
         if(segundos!=0){
             printf(", ");
         }
-    } else if (minutos == 1) {
-        printf("%d minuto", minutos);
-        if(segundos!=0){
-            printf(", ");
-        }
-    }
-    if (segundos > 0 && segundos > 1) {
-        printf("%d segundos", segundos);
+    } 
+    if (segundos > 0) {
+        printf("%d %s", segundos, segundos>1 ? "segundos" : "segundo");
     } else if (segundos == 1) {
         printf("%d segundo", segundos);
     }
